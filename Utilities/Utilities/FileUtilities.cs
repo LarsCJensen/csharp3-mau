d86f06d4897ba923c6366c553e6433ac8b2f2d3c
larscjensen <lars.c.jensen@gmail.com>
2022-10-16 21:40:20 +0200

    Separate Utilities lib

---
+++ Utilities/Utilities/FileUtilities.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace Utilities
+{
+    public static class FileUtilities
+    {
+        /// <summary>
+        /// Helper method to get logical drives 
+        /// </summary>
+        public static string[] AddLogicalDrives()
+        {
+            return Directory.GetLogicalDrives();            
+        }
+        /// <summary>
+        /// Helper method to get directories 
+        /// </summary>
+        public static string[] GetDirectories(string chosenDir)
+        {
+            return Directory.GetDirectories(chosenDir);
+        }
+        /// <summary>
+        /// Helper method to get files in directory. Uses distinct to get unique extensions.
+        /// </summary>
+        /// <param name="chosenDir">Folder to list files in</param>
+        /// <param name="extensions">List of extensions to check for</param>
+        /// <returns>List of strings</returns>
+        public static List<string> GetFilesInDirectory(string chosenDir, List<string> extensions = null)
+        {
+            try
+            {
+                if (extensions == null)
+                {
+                    return new List<string>(Directory.GetFiles(chosenDir));
+                }
+                else
+                {
+                    List<string> files = new List<string>();
+                    foreach (string ext in extensions.Distinct().ToList())
+                    {
+                        files.AddRange(Directory.GetFiles(chosenDir, "*"+ext));
+                    }
+                    return files;
+                }
+            } catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Unable to access files because of unauthorized exception", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return new List<string>();                
+            }
+            
+        }
+
+        public static List<FileInfo> GetFileInfoFromDirectory(string? chosenDir, List<string> extensions = null)
+        {
+            List<FileInfo> files = new List<FileInfo>();
+            foreach(string file in GetFilesInDirectory(chosenDir, extensions)) {
+                files.Add(new FileInfo(file));
+            }
+            return files;
+        }
+    }
+}
---
+++ Utilities/Utilities/VerifyPropertyException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    /// <summary>
+    /// Custom exception for when property can't be found
+    /// </summary>
+    public class VerifyPropertyException : Exception
+    {
+        public VerifyPropertyException() { }
+        public VerifyPropertyException(string message) : base(message) { }
+        public VerifyPropertyException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
